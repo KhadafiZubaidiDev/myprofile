39522b4b7d3a0ffcde4e34e4d27249ee705943d7
khadafizubaidi <khadafizubaidi.dev@gmail.com>
2024-06-10 06:20:05 +0800

    Create Model Pelatihan

--- my_profile_page/pelatihan/models.py
+++ my_profile_page/pelatihan/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Pelatihan(models.Model):
+  nama = models.CharField(max_length=255)
+  deskripsi = models.CharField(max_length=255)
+  penyelenggara = models.CharField(max_length=255)
+  tahun = models.CharField(max_length=4)
