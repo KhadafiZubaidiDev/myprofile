5302a6c4fe1d22dc7f558449146106eaf6392fd9
khadafizubaidi <khadafizubaidi.dev@gmail.com>
2024-06-11 06:40:19 +0800

    Change ALLOWED_HOSTS to *

--- my_profile_page/my_profile_page/settings.py
+++ my_profile_page/my_profile_page/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gfjn*u1zt$r2$klwas5enk@z!97@1!6ny3=6%1zahs&!$u%!z%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
